5d884c895ff6595e7b0224729d75b60b23378800
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-06-30 14:37:28 +0900

    update

--- lib/customUtilites.ts
+++ lib/customUtilites.ts
@@ -1,5 +1,5 @@
-import { Properties } from 'csstype';
-import { PluginAPI, CSSRuleObject } from 'tailwindcss/types/config';
+import { Properties } from "csstype";
+import { PluginAPI, CSSRuleObject } from "tailwindcss/types/config";
 
 type CSSRulesType = CSSRuleObject & Properties;
 
@@ -7,15 +7,15 @@ export const customUtilities = (plugin: PluginAPI) => {
   const { addUtilities } = plugin;
 
   const utilities: { [key: string]: CSSRulesType } = {
-    '.custom-box-shadow': {
-      boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)',
+    ".custom-box-shadow": {
+      boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)",
     },
-    '.custom-hoge': {
-      fontSize: '10rem',
+    ".custom-hoge": {
+      fontSize: "10rem",
     },
-    '.h10': {
-      fontSize: '100rem',
-      animationTimeline: 'view()',
+    ".custom-fuga": {
+      fontSize: "100rem",
+      animationTimeline: "view()",
     },
   };
 
