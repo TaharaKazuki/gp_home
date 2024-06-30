1491a89232b81f8d89f811fbb28e22da004857b8
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-06-30 14:33:33 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div className=""></div>
+      <div className="custom-box-shadow"></div>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,63 +1,63 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
-import { customUtilities, customUtilities2 } from './lib/customUtilites';
+import { customUtilities } from "./lib/customUtilites";
 
 const config = {
-  darkMode: ['class'],
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
-  prefix: '',
+  prefix: "",
   theme: {
     container: {
       center: true,
-      padding: '15px',
+      padding: "15px",
     },
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1200px',
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
     },
     fontFamily: {
-      primary: 'var(--font-marcellus)',
-      secondary: 'var(--font-montserrat)',
-      sans: ['var(--font-noto-sans-jp)'],
+      primary: "var(--font-marcellus)",
+      secondary: "var(--font-montserrat)",
+      sans: ["var(--font-noto-sans-jp)"],
     },
     extend: {
       colors: {
         primary: {
-          DEFAULT: '#473936',
+          DEFAULT: "#473936",
         },
         secondary: {
-          DEFAULT: '#f2dfce',
-          100: '#cea39c',
+          DEFAULT: "#f2dfce",
+          100: "#cea39c",
         },
         accent: {
-          DEFAULT: '#f19687',
-          100: '#f2d5c5',
+          DEFAULT: "#f19687",
+          100: "#f2d5c5",
         },
       },
       keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), customUtilities, customUtilities2],
+  plugins: [require("tailwindcss-animate"), customUtilities],
 } satisfies Config;
 
 export default config;
