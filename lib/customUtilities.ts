1c7b7776e7982dca5a840786a91289440ef86495
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-03 13:20:27 +0900

    update

--- lib/customUtilities.ts
+++ lib/customUtilities.ts
@@ -4,13 +4,36 @@ import { PluginAPI, CSSRuleObject } from 'tailwindcss/types/config';
 type CSSRulesType = CSSRuleObject & Properties;
 
 export const customUtilities = (plugin: PluginAPI) => {
-  const { addUtilities } = plugin;
+  const { addUtilities, theme } = plugin;
+
+  const accentColor = theme('colors.accent.DEFAULT') as string | undefined;
+  const fontFamilyPrimary = theme('fontFamily.primary');
+
+  const fontFamilyPrimaryString = Array.isArray(fontFamilyPrimary)
+    ? fontFamilyPrimary.join(', ')
+    : typeof fontFamilyPrimary === 'string'
+    ? fontFamilyPrimary
+    : 'YourFontFamily';
 
   const utilities: { [key: string]: CSSRulesType } = {
-    '.custom-box-shadow': {
-      boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)',
+    '.btn': {
+      backgroundColor: accentColor || '#f19687',
+      'min-width': '178px',
+      'border-top-left-radius': '30px',
+      'border-bottom-left-radius': '30px',
+      'border-top-right-radius': '4px',
+      'border-bottom-right-radius': '24px',
+      'font-family': fontFamilyPrimaryString,
+      'text-transform': 'uppercase',
+      'letter-spacing': '1px',
+      height: '58px',
+      'padding-left': '2rem',
+      'padding-right': '1.5rem',
+      display: 'flex',
+      'align-items': 'center',
+      'justify-content': 'center',
+      color: '#fff',
     },
   };
-
   addUtilities(utilities);
 };
