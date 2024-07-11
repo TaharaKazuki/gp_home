40c5ba3e02abcd0f3ae5e56e67959710f9eb596f
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-07-12 08:50:55 +0900

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,12 @@
 'use client';
 
-import { motion } from 'framer-motion';
-import React, { useContext } from 'react';
-
-import { CursorContext } from '@/app/provider/CursorContext';
+import React from 'react';
 
 const Header = () => {
-  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
-
   return (
-    <motion.header
-      onMouseEnter={mouseEnterHandler}
-      onMouseLeave={mouseLeaveHandler}
-      className="p-24"
-    >
+    <header className="fixed z-40 w-full bg-accent-100 pb-6 xl:bg-transparent xl:pb-[50px]">
       header
-    </motion.header>
+    </header>
   );
 };
 
