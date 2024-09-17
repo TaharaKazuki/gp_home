e17ac4f63014f2f021566f08124e5149b7919dc3
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-09-17 14:21:55 +0900

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,20 +3,59 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { IoMdMail } from 'react-icons/io';
 
 import MobileNav from './MobileNav';
 import Nav from './Nav';
+import Socials from './Socials';
+import { CursorContext } from '@/provider/CursorContext';
 
 const Header = () => {
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+
   const [mobileNav, setMobileNav] = useState(false);
 
   return (
     <header className="fixed z-40 w-full bg-accent-100 pb-6 xl:bg-transparent xl:pb-[50px]">
-      <div className="mb-6 bg-secondary-100 py-4 xl:mb-[50px] xl:h-[50px] xl:py-8"></div>
+      <div className="mb-6 bg-secondary-100 py-4 xl:mb-[50px] xl:h-[50px] xl:py-8">
+        <div className="container mx-auto h-full">
+          <div className="flex h-full items-center justify-between">
+            <motion.div
+              onMouseEnter={mouseEnterHandler}
+              onMouseLeave={mouseLeaveHandler}
+              className="flex size-full flex-col items-center justify-between gap-2 lg:flex-row xl:w-auto xl:justify-normal xl:gap-6"
+            >
+              {/* phone */}
+              <div className="flex items-center gap-2 text-white">
+                <FaPhoneAlt />
+                <span>090 0000 0000</span>
+              </div>
+              {/* email */}
+              <div className="flex items-center gap-2 text-white">
+                <IoMdMail />
+                <span>sample@co.jp</span>
+              </div>
+            </motion.div>
+            {/* socials */}
+            <motion.div
+              onMouseEnter={mouseEnterHandler}
+              onMouseLeave={mouseLeaveHandler}
+              className="hidden xl:block"
+            >
+              <Socials containerStyles="flex gap-6 text-xl text-white" />
+            </motion.div>
+          </div>
+        </div>
+      </div>
       <div className="container mx-auto flex items-center justify-between px-6">
-        <div>
+        {/* logo */}
+        <motion.div
+          onMouseEnter={mouseEnterHandler}
+          onMouseLeave={mouseLeaveHandler}
+        >
           <Link href="/">
             <Image
               src="assets/logo.svg"
@@ -27,7 +66,7 @@ const Header = () => {
               alt=""
             />
           </Link>
-        </div>
+        </motion.div>
         <div
           className="cursor-pointer xl:hidden"
           onClick={() => setMobileNav(true)}
@@ -48,9 +87,13 @@ const Header = () => {
           <MobileNav setMobileNav={setMobileNav} />
         </motion.div>
         {/* desktop nav */}
-        <div className="hidden xl:block">
+        <motion.div
+          onMouseEnter={mouseEnterHandler}
+          onMouseLeave={mouseLeaveHandler}
+          className="hidden xl:block"
+        >
           <Nav />
-        </div>
+        </motion.div>
       </div>
     </header>
   );
