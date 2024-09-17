bb4a9beee42e5f4c2641ecfe6922f0f25cc63e9c
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-09-17 15:28:47 +0900

    update

--- components/ModalVideo.tsx
+++ components/ModalVideo.tsx
@@ -19,7 +19,6 @@ const ModalVideo = () => {
           width={'100%'}
           height={'100%'}
           url="https://www.youtube.com/watch?v=0fGzpgqmLFY"
-          muted
         />
       </DialogContent>
     </Dialog>
